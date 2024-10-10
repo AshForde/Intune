bdb39fff981975b85ca79cdbb175ac7de6b8bd80
Ash Forde <Ashley.Forde@hud.govt.nz>
2024-10-10 16:38:13 +1300

    Refactor script to remove unused PowerShell scripts

--- _Projects/HUD Digital Support/Authentication_Snippets.ps1
+++ _Projects/HUD Digital Support/Authentication_Snippets.ps1
@@ -1,44 +1,76 @@
-# Requires -Modules Microsoft.Graph.Authentication
+#______________________________________________________________________________________________________________________________________________________
 # Requires -Modules ExchangeOnlineManagement
+# Requires -Modules Microsoft.Graph.Authentication
 # Requires -Modules PNP.Powershell
 # Requires -Modules MicrosoftTeams
+#______________________________________________________________________________________________________________________________________________________
 
 # Function to connect to Microsoft Graph
-function Connect-MicrosoftGraph {
-    Write-Host "Connecting to Microsoft Graph..."
-    Connect-MgGraph -ClientId $env:DigitalSupportAppID `
-                    -TenantId $env:DigitalSupportTenantID `
-                    -CertificateThumbprint $env:DigitalSupportCertificateThumbprint `
-                    -NoWelcome | Out-Null
-    Write-Host "Connected to Microsoft Graph."
+try {
+    Connect-MgGraph `
+        -ClientId $env:DigitalSupportAppID `
+        -TenantId $env:DigitalSupportTenantID `
+        -CertificateThumbprint $env:DigitalSupportCertificateThumbprint `
+        -NoWelcome
+    Write-Host "Connected to Graph" -ForegroundColor Green
+
+    $CollectToken = Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/v1.0/users" -ContentType "txt" -OutputType HttpResponseMessage
+    $Token        = $CollectToken.RequestMessage.Headers.Authorization.Parameter
+    $Token | Out-Null
+
+    } catch {
+        Write-Host "Error connecting to Microsoft Graph. Please check your credentials and network connection." -ForegroundColor Red
+        exit 1
 }
+#______________________________________________________________________________________________________________________________________________________
 
 # Function to connect to Exchange Online
-function Connect-ExchangeOnlineService {
-    Write-Host "Connecting to Exchange Online..."
-    Connect-ExchangeOnline -AppId $env:DigitalSupportAppID `
-                           -Organization "mhud.onmicrosoft.com" `
-                           -CertificateThumbprint $env:DigitalSupportCertificateThumbprint `
-                           -ShowBanner:$false
-    Write-Host "Connected to Exchange Online."
+try{
+    Connect-ExchangeOnline `
+        -AppId $env:DigitalSupportAppID `
+        -Organization "mhud.onmicrosoft.com" `
+        -CertificateThumbprint $env:DigitalSupportCertificateThumbprint `
+        -ShowBanner:$false
+    Write-Host "Connected to Exchange Online." -ForegroundColor Green
+    
+    } catch {
+        Write-Host "Error connecting to Exchange Online. Please check your credentials and network connection." -ForegroundColor Red
+        exit 1
 }
 
-# Function to connect to PnP PowerShell
-function Connect-PnPPowerShell {
-    Write-Host "Connecting to PnP PowerShell..."
-    Connect-PnPOnline -Url "https://mhud.sharepoint.com" `
-                      -ClientId $env:DigitalSupportAppID `
-                      -Tenant 'mhud.onmicrosoft.com' `
-                      -Thumbprint $env:DigitalSupportCertificateThumbprint
-    Write-Host "Connected to PnP PowerShell."
+#______________________________________________________________________________________________________________________________________________________
+
+# Function to connect to SharePoint Online
+if ($PSVersionTable.PSVersion -gt [Version]"7.0") {
+    try {
+        # Disable PnP PowerShell update check
+        $env:PNPPOWERSHELL_UPDATECHECK = "Off"
+        
+        Connect-PnPOnline `
+            -Url "https://mhud.sharepoint.com" `
+            -ClientId $env:DigitalSupportAppID `
+            -Tenant 'mhud.onmicrosoft.com' `
+            -Thumbprint $env:DigitalSupportCertificateThumbprint
+        Write-Host "Connected to PnP PowerShell."  -ForegroundColor Green
+        
+    } catch {
+        Write-Host "Error connecting to PNP SharePoint Online. Please check your credentials and network connection." -ForegroundColor Red
+        exit 1
+    }
+} else {
+    Write-Host "PowerShell version is below 7.2. Skipping PnP PowerShell connection." -ForegroundColor Yellow
 }
+#______________________________________________________________________________________________________________________________________________________
 
 # Function to connect to Microsoft Teams
-function Connect-Teams {
-    Write-Host "Connecting to Microsoft Teams..."
+try {
     Connect-MicrosoftTeams `
         -TenantId $env:DigitalSupportTenantID `
         -CertificateThumbprint $env:DigitalSupportCertificateThumbprint `
-        ApplicationId $env:DigitalSupportAppID
-    Write-Host "Connected to Microsoft Teams."
+        -ApplicationId $env:DigitalSupportAppID | Out-Null
+    Write-Host "Connected to Microsoft Teams." -ForegroundColor Green
+    
+    } catch {
+        Write-Host "Error connecting to Microsoft Teams. Please check your credentials and network connection." -ForegroundColor Red
+        exit 1
 }
